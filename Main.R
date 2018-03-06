a0f2576ceac580e2b94309a6d08789633c524332
EliasLundell <elias.lundell@skola.taby.se>
2018-03-06 16:54:19 +0100

    Added image-desctiption code for the Main.R file

--- Main.R
+++ Main.R
@@ -7,13 +7,13 @@ setwd("~/repos/iceland-flora/")
 
 # is = isländska
 
-# https://www.w3schools.com/cssref/css_selectors.asp 
+# https://www.w3schools.com/cssref/css_selectors.asp
 
 # Definiera de variablar som är globala och används på flera ställen
 
 main_site_url <- "http://www.floraislands.is/"
 
-blommor_subsite_url = "blom.html"
+blommor_subsite_url <- "blom.html"
 ormbunkar_subsite_url <- "burknar.html"
 mosor_subsite_url <- "mosar.html"
 lavar_subsite_url <- "flettur.html"
@@ -23,12 +23,13 @@ load_all_data <- FALSE
 
 # Funktions definiering --------------------------------------------------------------------------------------------------------------
 
-# Få alla latinska namn från en växts sub-sida
-extract_name_latin <- function(html_object) {
-  res <- html_object %>%
+# Få alla latinska namn från en växts sub-sida ------------------------------
+extract_name_latin <- function(url) {
+  res <- url %>%
+    read_html() %>%
     html_nodes("#page_content h2") %>%
     html_text() %>%
-    str_replace("\n\t\t", "")
+    str_remove_all("\n\t\t")
   
   return (res)
 }
@@ -36,16 +37,37 @@ extract_name_latin <- function(html_object) {
 # Lägg till en "possibly" till "extract_name_latin" funktionen
 extract_name_latin_possibly <- possibly(extract_name_latin, NA)
 
-# Få alla bilder från en växts sub-sida
-extract_images <- function(html_object) {
-  res <- html_object %>%
-    html_nodes("rightCol > p > a[href]") %>%
+
+# Få alla bilder från en växts sub-sida ------------------------------
+extract_images <- function(url) {
+  res <- url %>%
+    read_html() %>%
+    html_nodes("#right_col > p > a[href]") %>%
     html_attr("href")
   
   return (res)
 }
 
 # Lägg till en "possibly" till "extract_images" funktionen
+extract_images_possibly <- possibly(extract_images, NA)
+
+# Få alla bild-beskrivningar/ bild-texter ------------------------------
+extract_image_desciptions <- function(url) {
+  res <- url %>%
+    read_html() %>%
+    html_nodes("#right_col > p") %>%
+    html_text() %>%
+    str_remove_all("\n") %>%
+    str_remove_all("\t") %>%
+    str_trim()
+  
+  res <- res[res != ""]
+
+  return (res)
+}
+
+# Lägg till en possibly till "extract_image_descriptions" funktionen
+extract_image_desciptions_possibly <- possibly(extract_image_desciptions, NA)
 
 
 # Få information från en sub-sida (t.ex. blommor, mosor, ormbunkar mm)
@@ -93,11 +115,16 @@ get_information_from_subsite <- function(site_url, subsite_url) {
   # Lägg till huvudhemsidan till html länkarna
   urls <- paste0(site_url, vaxter_html)
   
+  # Läs in alla latinska namn
   vaxter_namn_latin <- map_chr(urls, extract_name_latin_possibly)
   
-  bilder <- map_chr(urls, extract_images_possibly)
+  # Läs in alla bilder
+  bilder <- map(urls, extract_images_possibly)
   
-  return (list("latinska_namn" = vaxter_namn_latin, "islandska_namn" = vaxter_namn_is, "bilder" = bilder))
+  # Läs in alla bild-texter
+  bild_texter <- map(urls, extract_image_desciptions_possibly)
+  
+  return (list("latinska_namn" = vaxter_namn_latin, "islandska_namn" = vaxter_namn_is, "bilder" = bilder, "bild_texter" = bild_texter))
 }
 
 # Börja ladda hemsidan ---------------------------------------------------------------------------------------------------------------
@@ -110,10 +137,10 @@ if (load_all_data) {
   svampar_information <- get_information_from_subsite(main_site_url, svampar_subsite_url)
   
   bio_information <- list(
-    "blommor" = blommor_information, 
-    "ormbunkar" = ormbunkar_information, 
-    "mosor" = mosor_information, 
-    "lavar" = lavar_information, 
+    "blommor" = blommor_information,
+    "ormbunkar" = ormbunkar_information,
+    "mosor" = mosor_information,
+    "lavar" = lavar_information,
     "svampar" = svampar_information
   )
 }
@@ -126,7 +153,7 @@ if (load_all_data) {
 
 
 # test_information <- get_information_from_subsite(main_site_url, blommor_subsite_url)
-# test_information <- get_information_from_subsite(main_site_url, ormbunkar_subsite_url)
+test_information <- get_information_from_subsite(main_site_url, ormbunkar_subsite_url)
 # test_information <- get_information_from_subsite(main_site_url, mosor_subsite_url)
 # test_information <- get_information_from_subsite(main_site_url, lavar_subsite_url)
 # test_information <- get_information_from_subsite(main_site_url, svampar_subsite_url)
@@ -144,5 +171,10 @@ if (load_all_data) {
 
 # get("islandska_namn", test_information)
 
+# " ---------------------------------------- Bilder ---------------------------------------- "
+# get("bilder", test_information)
+
+" ---------------------------------------- Bildtexter ---------------------------------------- "
+get("bild_texter", test_information)
 
 
